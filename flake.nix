23838600486623add09bbec5c9fd047f63b124ac
Flexksx <cristian.cretu2808@gmail.com>
2024-11-01 11:54:56 +0200

    add bs

--- flake.nix
+++ flake.nix
@@ -24,17 +24,34 @@
             pkgs.openjdk17
             pkgs.maven
             pkgs.nodejs
-            pkgs.postgresql
             pkgs.docker
+            pkgs.docker-compose
           ];
+
           shellHook = ''
             export DB_HOST=localhost
             export DB_PORT=5432
             export DB_USER=postgres
             export DB_PASSWORD=password
             export NODE_ENV=development
-            export JAVA_HOME=${pkgs.openjdk11}
+            export JAVA_HOME=${pkgs.openjdk17}
+            echo "[SUCCESS] Added envirnoment variables OneDollarBid is ready!"
+            echo "[DOCKER] Starting Dockerized PostgreSQL..."
+            docker-compose up -d
+
             echo "Development environment for OneDollarBid is ready!"
+            echo "Environment variables:"
+            echo "  DB_HOST: $DB_HOST"
+            echo "  DB_PORT: $DB_PORT"
+            echo "  DB_USER: $DB_USER"
+            echo "  DB_PASSWORD: $DB_PASSWORD"
+            echo "  NODE_ENV: $NODE_ENV"
+            echo "  JAVA_HOME: $JAVA_HOME"
+          '';
+
+          shellExitHook = ''
+            echo "Stopping Dockerized PostgreSQL..."
+            docker-compose down
           '';
         };
       }
